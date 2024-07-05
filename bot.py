4d4c90483a958753f4e12f4fc7fef9422a450107
OSINTAgency <147718706+OSINTAgency@users.noreply.github.com>
2024-07-05 10:19:02 +0000

    fon

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, Dispatcher
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot, BotCommand
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from flask import Flask, request
 import os
 import requests
@@ -15,15 +15,59 @@ from utils.whois import search_whois, is_valid_domain # Import is_valid_domain
 from utils.nslookup import nslookup_query
 
 # Configure logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Affiche les logs dans la console
+        logging.FileHandler('bot.log')  # Enregistre les logs dans un fichier
+    ]
+)
+
 logger = logging.getLogger(__name__)
 
 # Initialiser Flask
 app = Flask(__name__)
 
-# Initialiser le bot Telegram
-bot = Bot(Config.TELEGRAM_BOT_TOKEN)
-dispatcher = Dispatcher(bot, None, use_context=True)
+# Initialiser le bot Telegram avec Updater
+updater = Updater(Config.TELEGRAM_BOT_TOKEN)
+dispatcher = updater.dispatcher
+bot = updater.bot
+
+# Définir les commandes et obtenir le clavier interactif
+def set_commands_and_keyboard(bot):
+    # Définir les commandes pour l'autosaisie
+    bot.set_my_commands([
+        BotCommand("start", "Démarrer le bot"),
+        BotCommand("help", "Afficher les commandes disponibles"),
+        BotCommand("search_breaches", "Rechercher des violations de données"),
+        BotCommand("search_ip", "Rechercher des informations sur une adresse IP"),
+        BotCommand("search_whois", "Rechercher des informations WHOIS"),
+        BotCommand("search_twitter", "Recherche sur Twitter"),
+        BotCommand("host", "Utilise l'outil host"),
+        BotCommand("nslookup", "Utilise l'outil nslookup"),
+        BotCommand("dnseum", "Utilise l'outil dnseum"),
+        BotCommand("tld_expand", "Utilise l'outil tld-expand"),
+        BotCommand("pay_with_coinbase", "Payer avec Coinbase Commerce"),
+    ])
+
+    # Créer un clavier interactif pour les fonctions principales
+    keyboard = [
+        [InlineKeyboardButton("Rechercher des violations de données", callback_data='search_breaches')],
+        [InlineKeyboardButton("Rechercher des informations sur une adresse IP", callback_data='search_ip')],
+        [InlineKeyboardButton("Rechercher des informations WHOIS", callback_data='search_whois')],
+        [InlineKeyboardButton("Recherche sur Twitter", callback_data='search_twitter')],
+        [InlineKeyboardButton("Utiliser l'outil host", callback_data='host')],
+        [InlineKeyboardButton("Utiliser l'outil nslookup", callback_data='nslookup')],
+        [InlineKeyboardButton("Utiliser l'outil dnseum", callback_data='dnseum')],
+        [InlineKeyboardButton("Utiliser l'outil tld-expand", callback_data='tld_expand')],
+        [InlineKeyboardButton("Payer avec Coinbase Commerce", callback_data='pay_with_coinbase')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    return reply_markup
+
+reply_markup = set_commands_and_keyboard(bot)
 
 @app.route('/')
 def hello():
@@ -38,9 +82,8 @@ def webhook():
         update = Update.de_json(data, bot)
         dispatcher.process_update(update)
     except Exception as e:
-          logger.error(f"Error processing webhook: {e}")
+        logger.error(f"Error processing webhook: {e}")
     return 'ok'
-
 
 # Définir l'URL du webhook
 set_webhook_url = f"https://api.telegram.org/bot{Config.TELEGRAM_BOT_TOKEN}/setWebhook?url={Config.APP_URL}/{Config.TELEGRAM_BOT_TOKEN}"
@@ -50,9 +93,15 @@ if response.status_code == 200:
 else:
     print(f"Failed to set webhook: {response.text}")
 
-# Fonction de démarrage
+# Fonction de démarrage améliorée avec clavier interactif
 def start_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Bonjour! Utilisez /help pour voir les commandes disponibles.')
+    welcome_message = (
+        "👋 Bonjour et bienvenue sur CyberDetectiveBot! 🕵️‍♂️\n\n"
+        "Ce bot vous permet de réaliser des recherches OSINT directement depuis Telegram.\n\n"
+        "Utilisez les boutons ci-dessous pour commencer."
+    )
+    update.message.reply_text(welcome_message, reply_markup=reply_markup)
+
 
 # Fonction d'aide
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -73,7 +122,7 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 # Commandes spécifiques
 def search_twitter_command(update: Update, context: CallbackContext) -> None:
-    search_twitter(update, context) # search_twitter not defined
+    search_twitter(update, context)  # search_twitter not defined
 
 def search_whois_command(update: Update, context: CallbackContext) -> None:
     logger.info("Entered search_whois function")
@@ -102,25 +151,23 @@ def search_whois_command(update: Update, context: CallbackContext) -> None:
         logger.error(f"Erreur lors de la recherche Whois: {e}")
         update.message.reply_text(f"Erreur Whois: {str(e)}")
 
-
-
 def search_ip_command(update: Update, context: CallbackContext) -> None:
-    search_ip(update, context) # search_ip not defined
+    search_ip(update, context)  # search_ip not defined
 
 def search_breaches_command(update: Update, context: CallbackContext) -> None:
     search_breaches(update, context)
 
 def host_command(update: Update, context: CallbackContext) -> None:
-    host_lookup(update, context) # host_lookup not defined
+    host_lookup(update, context)  # host_lookup not defined
 
 def nslookup_command(update: Update, context: CallbackContext) -> None:
     nslookup_query(update, context)
 
 def dnseum_command(update: Update, context: CallbackContext) -> None:
-    dnseum_query(update, context) # dnseum_query not defined
+    dnseum_query(update, context)  # dnseum_query not defined
 
 def tld_expand_command(update: Update, context: CallbackContext) -> None:
-    tld_expand_query(update, context) # tld_expand_query not defined
+    tld_expand_query(update, context)  # tld_expand_query not defined
 
 # Fonction de paiement avec Coinbase Commerce
 def pay_with_coinbase(update: Update, context: CallbackContext) -> None:
@@ -145,10 +192,11 @@ def pay_with_coinbase(update: Update, context: CallbackContext) -> None:
             cancel_url=f"{Config.APP_URL}/cancel",
         )
 
+    
         update.message.reply_text(f"Veuillez procéder au paiement en cliquant sur le lien suivant : {charge['hosted_url']}")
     except Exception as e:
         update.message.reply_text(f"Erreur lors de la création de la charge de paiement : {str(e)}")
-
+        
 # Ajout des gestionnaires de commandes au dispatcher
 dispatcher.add_handler(CommandHandler("start", start_command))
 dispatcher.add_handler(CommandHandler("help", help_command))
@@ -162,24 +210,11 @@ dispatcher.add_handler(CommandHandler("dnseum", dnseum_command))
 dispatcher.add_handler(CommandHandler("tld_expand", tld_expand_command))
 dispatcher.add_handler(CommandHandler("pay_with_coinbase", pay_with_coinbase))
 
-# Définir les commandes pour l'autosaisie
-bot.set_my_commands([
-    BotCommand("start", "Démarrer le bot"),
-    BotCommand("help", "Afficher les commandes disponibles"),
-    BotCommand("search_breaches", "Rechercher des violations de données"),
-    BotCommand("search_ip", "Rechercher des informations sur une adresse IP"),
-    BotCommand("search_whois", "Rechercher des informations WHOIS"),
-    BotCommand("search_twitter", "Recherche sur Twitter"),
-    BotCommand("host", "Utilise l'outil host"),
-    BotCommand("nslookup", "Utilise l'outil nslookup"),
-    BotCommand("dnseum", "Utilise l'outil dnseum"),
-    BotCommand("tld_expand", "Utilise l'outil tld-expand"),
-    BotCommand("pay_with_coinbase", "Payer avec Coinbase Commerce"),
-])
+
 # Ajout d'un gestionnaire pour les erreurs
 def error(update: Update, context: CallbackContext) -> None:
     """Log the error et notify the user."""
-    logging.warning(f'Update "{update}" caused error "{context.error}"')
+    logger.warning(f'Update "{update}" caused error "{context.error}"')
     update.message.reply_text('Une erreur est survenue. Veuillez réessayer plus tard.')
 
 dispatcher.add_error_handler(error)
