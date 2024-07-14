67a9fe92ca6aaf7700e848af8a6ba5a2ca8f7baf
OSINTAgency <147718706+OSINTAgency@users.noreply.github.com>
2024-07-14 16:26:05 +0000

    Conf

--- set_webhook.py
+++ set_webhook.py
@@ -3,9 +3,9 @@ import os
 
 # Remplacez par votre URL Heroku
 heroku_app_url = "https://cyberdetectivebot-44a828f078dc.herokuapp.com/"
-
+5
 # Remplacez par votre token de bot Telegram
-telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')  # Ou remplacez par votre token directement
+telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')  # Ou remplacez par votre token directement 
 if not telegram_bot_token:
     raise ValueError("TELEGRAM_BOT_TOKEN is not set in environment variables")
 
